23acb29898850f36b4c24a7b991ce04038b255b3
Lusanda Tsilana <124098032+LusandaTsilana@users.noreply.github.com>
2023-11-27 15:36:09 +0200

    ignore

--- GetUpApp/server/Routes/UserRoute.js
+++ GetUpApp/server/Routes/UserRoute.js
@@ -10,6 +10,7 @@ const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+
 router.get("/find/:userId", findUser);
 router.get("/", getUsers);
 
