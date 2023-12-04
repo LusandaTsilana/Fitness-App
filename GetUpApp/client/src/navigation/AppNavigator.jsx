595a3c48bbe76e5493ec82c485a8bc143b6dbc36
Lusanda Tsilana <124098032+LusandaTsilana@users.noreply.github.com>
2023-12-04 14:45:21 +0200

    ignore

--- GetUpApp/client/src/navigation/AppNavigator.jsx
+++ GetUpApp/client/src/navigation/AppNavigator.jsx
@@ -1,38 +1,52 @@
 import React from "react";
+import { View } from "react-native";
 
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
 
 import Today from "../screens/Today";
 import Workout from "../screens/Workout";
 import Community from "../screens/Community";
 import Profile from "../screens/Profile";
 
-const Stack = createNativeStackNavigator();
+//icons
+import Icon from "react-native-vector-icons/Ionicons";
+
+// const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+const screenOptions = {
+  tabBarShowLabel: false,
+  headerShown: false,
+  tabBarStyle: {
+    position: "absolute",
+    bottom: 0,
+    right: 0,
+    left: 0,
+    elevation: 0,
+    height: 60,
+    background: "green",
+  },
+};
 
 const AppNavigator = () => {
   return (
-    <Stack.Navigator>
-      <Stack.Screen
+    <Tab.Navigator screenOptions={screenOptions}>
+      <Tab.Screen
         name="Today"
         component={Today}
-        options={{ headerShown: false }}
+        options={{
+          tabBarIcon: ({ focused }) => {
+            <View style={{ alignItems: "center", justifyContent: "center" }}>
+              <Icon name="home-outline" size={30} color="black" />
+            </View>;
+          },
+        }}
       />
-      <Stack.Screen
-        name="Workout"
-        component={Workout}
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-        name="Community"
-        component={Community}
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-        name="Profile"
-        component={Profile}
-        options={{ headerShown: false }}
-      />
-    </Stack.Navigator>
+      <Tab.Screen name="Workout" component={Workout} />
+      <Tab.Screen name="Community" component={Community} />
+      <Tab.Screen name="Profile" component={Profile} />
+    </Tab.Navigator>
   );
 };
 
