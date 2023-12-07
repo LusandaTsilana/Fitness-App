c0437d1e0d6c209ffdc6bf2d7bad9e294ffb200e
Lusanda Tsilana <124098032+LusandaTsilana@users.noreply.github.com>
2023-12-07 11:36:31 +0200

    ignore

--- GetUpApp/client/src/screens/Today.jsx
+++ GetUpApp/client/src/screens/Today.jsx
@@ -4,13 +4,18 @@ import React from "react";
 //components
 import AppHeader from "../components/AppHeader";
 import ImageSlider from "../components/ImageSlider";
+import TextComponent from "../components/TextComponent";
 
 const Today = () => {
   return (
-    <View style={{ backgroundColor: "white" }}>
+    <View style={{ backgroundColor: "white", paddingHorizontal: 10 }}>
       <AppHeader content="Good day," showName={true} />
       <ScrollView style={styles.container}>
         <View>
+          <TextComponent
+            content="Explore"
+            style={{ textAlign: "left", fontSize: 24, marginLeft: 10 }}
+          />
           <ImageSlider />
         </View>
       </ScrollView>
