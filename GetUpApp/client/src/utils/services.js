eee8215e7eb9649e91ab89023b33f4941e5e48ff
Lusanda Tsilana <124098032+LusandaTsilana@users.noreply.github.com>
2023-12-13 11:32:01 +0200

    message API and ChatContext

--- GetUpApp/client/src/utils/services.js
+++ GetUpApp/client/src/utils/services.js
@@ -26,3 +26,21 @@ export const postRequest = async (url, body) => {
 
   return data;
 };
+
+//get request creation to get data from backend
+export const getRequest = async (url) => {
+  const response = await fetch(url);
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    let message = "An error has occured";
+
+    if (data?.message) {
+      message = data.message;
+    }
+    return { error: true, message };
+  }
+
+  return data;
+};
